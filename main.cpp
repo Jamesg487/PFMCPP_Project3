60aad2e6eb4ca25da7ef683d3bb750dccdaa210b
Jamesg487 <jamesg487@gmail.com>
2020-06-26 23:22:32 +0000

    Corrected indent issues

--- main.cpp
+++ main.cpp
@@ -113,7 +113,6 @@ struct CarWash //                                   1) define an empty struct fo
     Car carBeingServiced;  
 };
 
-
 /*
 Thing 1) Electric Guitar
 5 properties:
@@ -129,24 +128,25 @@ Thing 1) Electric Guitar
  */
 
 struct ElectricGuitar
-{
-//pickups 
-std::string pickups = "Humbucker";
-//number of stings
-int numOfStrings = 6;
-//body material 
-std::string bodyMaterial= "Ash";
-//body style
-char bodyStyle = 'T';
-//fret size in inches
-float fretSize = 0.81f;
 
-//output audio
-void outputAudio (bool isJackConnected);
-//adjust tone
-int adjustTone (bool isVolumeKnob, int startKnobPosition, int endKnobPosition);
-//adjust volume
-int adjustVolume (bool isVolumeKnob, int startKnobPosition, int endKnobPosition); 
+{
+    //pickups 
+    std::string pickups = "Humbucker";
+    //number of stings
+    int numOfStrings = 6;
+    //body material 
+    std::string bodyMaterial= "Ash";
+    //body style
+    char bodyStyle = 'T';
+    //fret size in inches
+    float fretSize = 0.81f;
+
+    //output audio
+    void outputAudio (bool isJackConnected);
+    //adjust tone
+    int adjustTone (bool isVolumeKnob, int startKnobPosition, int endKnobPosition);
+    //adjust volume
+    int adjustVolume (bool isVolumeKnob, int startKnobPosition, int endKnobPosition); 
 };
 
 /*
@@ -164,24 +164,25 @@ Thing 2) Computer
  */
 
 struct Computer
-{
-//screen size in inches
-float screenSize = 13.3f;
-//usb port type
-char usbPortType = 'C';
-//ram size in GB
-int ramSize = 16;
-//screen resolution
-std::string screenRes = "1440×1080";
-//cpu in Ghz
-float cpu = 2.3f;
 
-//display using screen
-void displayWithScreen (bool isComputerOn);
-//output audio
-void outputAudio (bool isAudioPlaying);
-//connect to internet
-void connectToInternet (bool isRouterOn, std::string wifiPassword);
+{
+    //screen size in inches
+    float screenSize = 13.3f;
+    //usb port type
+    char usbPortType = 'C';
+    //ram size in GB
+    int ramSize = 16;
+    //screen resolution
+    std::string screenRes = "1440×1080";
+    //cpu in Ghz
+    float cpu = 2.3f;
+
+    //display using screen
+    void displayWithScreen (bool isComputerOn);
+    //output audio
+    void outputAudio (bool isAudioPlaying);
+    //connect to internet
+    void connectToInternet (bool isRouterOn, std::string wifiPassword);
 };
 
 /*
@@ -199,24 +200,25 @@ Thing 3) Bicycle
  */
 
 struct Bicycle
-{
-//wheels size in inches
-float wheeleSize = 27.5f;
-//frame size in cm
-int frameSize = 58;
-// number of gears
-int numOfGears = 21;
-//handlebar type
-std::string handlebarType = "DropDown";
-//tyres
-std::string tyres = "Road";
 
-//change gear
-void changeGear (int currentGear, int desiredGear);
-//steer
-bool steer (bool isObstacle, bool isDestinationDirection);
-//move
-bool move (bool onBike); 
+{
+    //wheels size in inches
+    float wheeleSize = 27.5f;
+    //frame size in cm
+    int frameSize = 58;
+    // number of gears
+    int numOfGears = 21;
+    //handlebar type
+    std::string handlebarType = "DropDown";
+    //tyres
+    std::string tyres = "Road";
+
+    //change gear
+    void changeGear (int currentGear, int desiredGear);
+    //steer
+    bool steer (bool isObstacle, bool isDestinationDirection);
+    //move
+    bool move (bool onBike); 
 };
 
 /*
@@ -234,24 +236,25 @@ Thing 4) Coffee Machine
  */
 
 struct CoffeeMachine
-{
-//portafilter size in mm
-int portafilterSize = 58;
-//water pressure in bar
-int waterPressure = 9;
-//hopper capacity in kg
-float hopperCapacity = 1.2f;
-//water reservoir capacity in ltr 
-double waterReservoirCapacity = 1.4;
-//grind coarseness
-std::string grindCoarseness = "Fine";
 
-//extract coffee
-void extractCoffee (bool isPortafilterFull, bool optimumWaterTemp);
-//froth milk
-void frothMilk (bool cappuccino, std::string milkType);
-//grind beans
-void grindBeans (std::string grindCoarseness);
+{
+    //portafilter size in mm
+    int portafilterSize = 58;
+    //water pressure in bar
+    int waterPressure = 9;
+    //hopper capacity in kg
+    float hopperCapacity = 1.2f;
+    //water reservoir capacity in ltr 
+    double waterReservoirCapacity = 1.4;
+    //grind coarseness
+    std::string grindCoarseness = "Fine";
+
+    //extract coffee
+    void extractCoffee (bool isPortafilterFull, bool optimumWaterTemp);
+    //froth milk
+    void frothMilk (bool cappuccino, std::string milkType);
+    //grind beans
+    void grindBeans (std::string grindCoarseness);
 };
 
 /*
@@ -269,24 +272,25 @@ Thing 5) Distortion Section
  */
 
 struct DistortionSection
-{
-//distortian type
-std::string distType = "Fuzz";
-//drive amount
-int driveAmount = 65;
-//tone
-int distTone = 40;
-//level
-int distLevel = 80;
-//distortion footswitch
-bool distFootswitchOn = true;
 
-//distort audio
-void distortAudio (int driveAmount, int distLevel, bool distFootswitchOn);
-//adjust volume
-int adjustVolume (int distLevel);
-//adjust brightness 
-int adjustBrightness (int distTone);
+{
+    //distortian type
+    std::string distType = "Fuzz";
+    //drive amount
+    int driveAmount = 65;
+    //tone
+    int distTone = 40;
+    //level
+    int distLevel = 80;
+    //distortion footswitch
+    bool distFootswitchOn = true;
+
+    //distort audio
+    void distortAudio (int driveAmount, int distLevel, bool distFootswitchOn);
+    //adjust volume
+    int adjustVolume (int distLevel);
+    //adjust brightness 
+    int adjustBrightness (int distTone);
 };
 
 /*
@@ -304,24 +308,25 @@ Thing 6) Delay Section
  */
 
 struct DelaySection
-{
-//delay type
-std::string delayType = "Short";
-//delay time in ms
-int delayTime = 110;
-//feedback amount
-int feedbackAmount = 42;
-//delay level
-int delayLevel = 30;
-//delay footswitch
-bool delayFootswitchOn = false;
 
-//add delayed audio signal
-void delayAudio (int delayTime, int feedbackAmount, bool delayFootswitchOn);
-//recieve tap information
-int tapTempo (bool delayFootswitchOn,int deltaTapTime);
-//adjust volume
-int adjustVolume (int delayLevel);
+{
+    //delay type
+    std::string delayType = "Short";
+    //delay time in ms
+    int delayTime = 110;
+    //feedback amount
+    int feedbackAmount = 42;
+    //delay level
+    int delayLevel = 30;
+    //delay footswitch
+    bool delayFootswitchOn = false;
+
+    //add delayed audio signal
+    void delayAudio (int delayTime, int feedbackAmount, bool delayFootswitchOn);
+    //recieve tap information
+    int tapTempo (bool delayFootswitchOn,int deltaTapTime);
+    //adjust volume
+    int adjustVolume (int delayLevel);
 };
 
 /*
@@ -339,24 +344,25 @@ Thing 7) Preamp
  */
 
 struct Preamp
-{
-//cab type
-std::string cabType = "VintageClean";
-//gain level
-int preampGain = 4;
-//treble value
-int preampTreble = 8;
-//bass value
-int preampBass = 5;
-//bypass switch
-bool isPreampBypassOn = false;
 
-//attenuate frequencies
-void outputEq (int preampTreble,int preampBass);
-//add overdrive
-void overdrive (int preampGain);
-//adjust volume
-int preampTotalVolume (int preampGain,int preampVolume); 
+{
+    //cab type
+    std::string cabType = "VintageClean";
+    //gain level
+    int preampGain = 4;
+    //treble value
+    int preampTreble = 8;
+    //bass value
+    int preampBass = 5;
+    //bypass switch
+    bool isPreampBypassOn = false;
+
+    //attenuate frequencies
+    void outputEq (int preampTreble,int preampBass);
+    //add overdrive
+    void overdrive (int preampGain);
+    //adjust volume
+    int preampTotalVolume (int preampGain,int preampVolume); 
 };
 
 /*
@@ -374,44 +380,47 @@ Thing 8) Modulation Section
  */
 
 struct ModulationSection 
-{
-//modulation type
-std::string modType = "Chorus";
-//rate amount
-int modRate = 15;
-//depth amount
-int modDepth = 34;
-//modulation level
-int modLevel = 73;
-//modulation footswitch
-bool modFootswitchOn = true;
 
-struct Harmoniser
 {
-//harmoniser key
-char harmKey = 'F';
-//pitch coarse
-int coarsePitch = -7;
-//pitch fine
-int finePitch = 55;
-//harmoniser mix
-int harmMix = 62;
-//harmoniser mode
-std::string harmMode = "Major";
+    //modulation type
+    std::string modType = "Chorus";
+    //rate amount
+    int modRate = 15;
+    //depth amount
+    int modDepth = 34;
+    //modulation level
+    int modLevel = 73;
+    //modulation footswitch
+    bool modFootswitchOn = true;
 
-//add harmomics
-void harmonise (int coarsePitch = -7, int finePitch = 55);
-//blend harmonics 
-void harmBlend (int harmMix = 62);
-//utilise scales
-void harmScale (char harmKey = 'F', std::string harmMode = "Major"); 
-};
-//add harmonic frequencies
-void harmFreq (Harmoniser harmParmeters, bool modFootswitchOn);
-//add dynamic modultaion
-void modDynamic (bool isTremoloOn,int modRate,int modDepth);
-//output phased audio
-void modPhasor (bool isPhasorOn,int modRate,int modDepth);
+    struct Harmoniser
+
+    {
+        //harmoniser key
+        char harmKey = 'F';
+        //pitch coarse
+        int coarsePitch = -7;
+        //pitch fine
+        int finePitch = 55;
+        //harmoniser mix
+        int harmMix = 62;
+        //harmoniser mode
+        std::string harmMode = "Major";
+
+        //add harmomics
+        void harmonise (int coarsePitch = -7, int finePitch = 55);
+        //blend harmonics 
+        void harmBlend (int harmMix = 62);
+        //utilise scales
+        void harmScale (char harmKey = 'F', std::string harmMode = "Major"); 
+    };
+
+    //add harmonic frequencies
+    void harmFreq (Harmoniser harmParmeters, bool modFootswitchOn);
+    //add dynamic modultaion
+    void modDynamic (bool isTremoloOn,int modRate,int modDepth);
+    //output phased audio
+    void modPhasor (bool isPhasorOn,int modRate,int modDepth);
 };
 
 /*
@@ -429,24 +438,25 @@ Thing 9) Display
  */
 
 struct Display
-{
-//height in cm
-float displayHeight = 2.4f;
-//width in cm
-float displayWidth = 3.65f;
-//digit size in inches
-double digitSize = 0.56;
-//brightness in ucd
-int displayBrightness = 12000;
-//number of LEDs
-int numLEDs = 16;
 
-//monitor battery level
-int batteryLevel (int getBatteryLevel);
-//display variables
-void displayParameter (bool hasKnobMoved, int knobValue);
-//display tuning information
-void displayTuning (char getAudioPitch, bool isSharp, bool isFlat);
+{
+    //height in cm
+    float displayHeight = 2.4f;
+    //width in cm
+    float displayWidth = 3.65f;
+    //digit size in inches
+    double digitSize = 0.56;
+    //brightness in ucd
+    int displayBrightness = 12000;
+    //number of LEDs
+    int numLEDs = 16;
+
+    //monitor battery level
+    int batteryLevel (int getBatteryLevel);
+    //display variables
+    void displayParameter (bool hasKnobMoved, int knobValue);
+    //display tuning information
+    void displayTuning (char getAudioPitch, bool isSharp, bool isFlat);
 };
 
 /*
@@ -464,44 +474,47 @@ Thing 10) Multi-effects Pedal
  */
 
 struct MultiEffectsPedal
-{
-//distortion section
-DistortionSection distParamValues();
-//delay section
-DelaySection delayParamValues();
-//preamp
-Preamp preampValues();
-//modulation section
-ModulationSection modParamValues();
-//display
-Display displayStatus();
 
-struct Looper
 {
-//looper engaged
-bool isLooperEngaged = true;
-//dub footswitch
-bool dubTrack = true;
-//track one level
-int trackOneLevel = 6;
-//dub track level
-int dubTrackLevel = 4;
-//tap tempo in ms
-int deltaTapTime = 441;
+    //distortion section
+    DistortionSection distParamValues();
+    //delay section
+    DelaySection delayParamValues();
+    //preamp
+    Preamp preampValues();
+    //modulation section
+    ModulationSection modParamValues();
+    //display
+    Display displayStatus();
 
-//blend looper tracks
-void looperMix (int trackOneLevel = 6, int dubTrackLevel = 4);
-//quantize audio
-void quantizeAudio (int bpm = 136, int deltaTapTime = 441);
-//overwrite loop
-void newLoop (bool isLooperEngaged = true);
-};
-//loop audio
-Looper loopParamValues();
-//modulate audio
-void audioModulation (ModulationSection modParamValues());
-//delay audio
-void audioDelay (DelaySection delayParamValues());
+    struct Looper
+
+    {
+        //looper engaged
+        bool isLooperEngaged = true;
+        //dub footswitch
+        bool dubTrack = true;
+        //track one level
+        int trackOneLevel = 6;
+        //dub track level
+        int dubTrackLevel = 4;
+        //tap tempo in ms
+        int deltaTapTime = 441;
+
+        //blend looper tracks
+        void looperMix (int trackOneLevel = 6, int dubTrackLevel = 4);
+        //quantize audio
+        void quantizeAudio (int bpm = 136, int deltaTapTime = 441);
+        //overwrite loop
+        void newLoop (bool isLooperEngaged = true);
+    };
+    
+    //loop audio
+    Looper loopParamValues();
+    //modulate audio
+    void audioModulation (ModulationSection modParamValues());
+    //delay audio
+    void audioDelay (DelaySection delayParamValues());
 };
 
 /*
